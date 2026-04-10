2b99143ccb9f5f82ce85677028f2b190ab0c4b32
preca-hoshino <001@labmem.com.cn>
2026-04-10 13:30:11 +0800

    [Add] shadcn UI Accordion and eslint rules bypass

--- src/router/__root.tsx
+++ src/router/__root.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-refresh/only-export-components */
 import { createRootRoute, Outlet } from '@tanstack/react-router';
 import { useEffect } from 'react';
 import { useAuthStore } from '@/stores/authStore';
