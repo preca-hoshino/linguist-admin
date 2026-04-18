c8a917385d479179a54475358799c56f3f7e1e4e
preca-hoshino <001@labmem.com.cn>
2026-04-18 19:06:15 +0800

    [Fix](lint): remove unnecessary Number() conversion in virtual-mcps-context

--- src/views/mcps/virtual-mcps/virtual-mcps-context.tsx
+++ src/views/mcps/virtual-mcps/virtual-mcps-context.tsx
@@ -79,7 +79,7 @@ export function VirtualMcpsProvider({
       const res = await listVirtualMcps(payload);
       if (res.ok) {
         setServers(res.data.data);
-        setTotal(Number(res.data.total));
+        setTotal(res.data.total);
         setHasMore(res.data.has_more);
       } else {
         setError(res.error.message);
