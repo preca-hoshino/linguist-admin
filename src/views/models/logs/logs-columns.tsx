72738601f0a575e800564e1b722bf35065f563c6
preca-hoshino <001@labmem.com.cn>
2026-04-26 22:59:57 +0800

    [Add](logs): Enable sorting for App column

--- src/views/models/logs/logs-columns.tsx
+++ src/views/models/logs/logs-columns.tsx
@@ -180,7 +180,7 @@ export function useLogsColumns(
 
         return <AppCell name={name} size="sm" />;
       },
-      enableSorting: false,
+      enableSorting: true,
     },
     {
       id: 'status',
