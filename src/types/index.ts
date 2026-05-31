31abad4ae256e50230c34addd2a0e4d99678b680
preca-hoshino <001@labmem.com.cn>
2026-05-31 17:00:26 +0800

    [Add](types): re-export permissions module from types barrel

--- src/types/index.ts
+++ src/types/index.ts
@@ -4,6 +4,7 @@ export * from './api';
 export * from './app';
 export * from './chat';
 export * from './health';
+export * from './permissions';
 export * from './provider';
 export * from './provider-model';
 export * from './request-log';
